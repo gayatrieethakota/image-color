314e61b64b78913c03da763086195f07cdd85486
gayatrieethakota <115683483+gayatrieethakota@users.noreply.github.com>
2024-01-09 16:55:41 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express();
+const mongoose =require('mongoose')
+const cors = require('cors')
+
+app.get('/',(req,res)=>{res.send("hello from server..")})
+
+app.use(cors())
+app.use(express.json())
+
+const Db = async()=>{
+    try {
+        await mongoose.connect('mongodb+srv://gayatrieethakota:gayatri2004@cluster0.krx4mdu.mongodb.net/colors?retryWrites=true&w=majority').then(
+            ()=>{
+                console.log("Connected to DataBase...")
+            }
+        )
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+Db();
+
+app.listen(2000,()=>{console.log("Server is running on port 2000..s.")})
